f499711d295cf969f94b2791c1509589c6d822c4
Orsolya Szocs <soie2628@scs.ubbcluj.ro>
2020-05-28 16:38:09 +0300

    modified conference menu

--- webapp/src/app/conference-menu/conference-menu.component.ts
+++ webapp/src/app/conference-menu/conference-menu.component.ts
@@ -3,6 +3,7 @@ import {UserService} from "../service/user-service";
 import {Router} from "@angular/router";
 import {User} from "../model/user";
 import {Conference} from "../model/conference";
+import {ProgramCommitteeMember} from "../model/program-committee-member";
 
 @Component({
   selector: 'app-conference-menu',
@@ -14,11 +15,25 @@ export class ConferenceMenuComponent implements OnInit {
   user: User;
   @Input()
   conference: Conference;
+  showRegisterPCM: boolean;
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService, private router: Router) {
+    this.user = this.userService.getCurrentUser();
+    this.showRegisterPCM = false;
+
+    /**/
+  }
 
   ngOnInit(): void {
-    this.user = this.userService.getCurrentUser();
+    console.log(this.conference.name);
+    this.conference.programCommittee.forEach(member => {
+      if (member.user.username === this.user.username && member.hasRegistered === false) {
+        this.showRegisterPCM = true;
+      }
+    });
+    if (this.conference === null) {
+      alert("null");
+    }
   }
 
 }
